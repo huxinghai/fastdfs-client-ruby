7b5470351ce9762014755b4f1d47150b7f29de5c
kaka <huxinghai1988@gmail.com>
2016-04-10 09:11:44 +0800

    fix test bug

--- lib/fastdfs-client/tracker.rb
+++ lib/fastdfs-client/tracker.rb
@@ -20,6 +20,7 @@ module Fastdfs
         header = ProtoCommon.header_bytes(@cmd, 0)
         @socket.write(@cmd, header)
         @socket.receive do |body|
+          debugger
           storage_ip = Utils.pack_trim(body[ProtoCommon::IPADDR])
           storage_port = body[ProtoCommon::PORT].unpack("C*").to_pack_long
           store_path = body[ProtoCommon::TRACKER_BODY_LEN-1].unpack("C*")[0]
