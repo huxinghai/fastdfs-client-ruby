80bcef767edc9893132390f77ba0596fade51959
kaka <huxinghai1988@gmail.com>
2016-04-12 10:27:49 +0800

    rename require main

---
+++ lib/fastdfs-client.rb
@@ -0,0 +1,7 @@
+require "fastdfs-client/extend_core"
+require 'fastdfs-client/socket'
+require 'fastdfs-client/cmd'
+require 'fastdfs-client/proto_common'
+require 'fastdfs-client/utils'
+require 'fastdfs-client/hook'
+require "fastdfs-client/tracker"
