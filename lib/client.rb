3ba9219ad1dfad3f873824d2bc3f4833831ad790
kaka <huxinghai1988@gmail.com>
2016-04-12 10:27:44 +0800

    rename require main

--- lib/client.rb
+++
@@ -1,7 +0,0 @@
-require "fastdfs-client/extend_core"
-require 'fastdfs-client/socket'
-require 'fastdfs-client/cmd'
-require 'fastdfs-client/proto_common'
-require 'fastdfs-client/utils'
-require 'fastdfs-client/hook'
-require "fastdfs-client/tracker"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'debugger'
 require 'rspec'
 require 'rspec/core'
 require 'rspec/mocks'
-require File.expand_path('../../lib/client', __FILE__)
+require File.expand_path('../../lib/fastdfs-client', __FILE__)
 require File.expand_path('../test_config', __FILE__)
 require File.expand_path('../mock_tcp_socket', __FILE__)
 
